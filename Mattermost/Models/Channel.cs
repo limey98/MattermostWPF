fc1d55dccdaeaafef4a7451cf90fc9de9868cc9d
limey98 <stephen.a.langstone@gmail.com>
2016-03-23 19:13:12 +0000

    Reworked channel cache to match user cache

--- Mattermost/Models/Channel.cs
+++ Mattermost/Models/Channel.cs
@@ -1,46 +1,31 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.Serialization;
 
 namespace Mattermost.Models
 {
     class Channel
     {
-        static Dictionary<string, Channel> channelList = new Dictionary<string, Channel>();
+        static List<Channel> channelList = new List<Channel>();
 
         public static Channel GetChannelByID(string id)
         {
-            if (!channelList.ContainsKey(id))
-                return null;
-
-            return channelList[id];
+            return channelList.FirstOrDefault(c => c.ID == id);
         }
 
-        string id;
-
-        public string ID
-        {
-            get { return id; }
-            set
-            {
-                if (id == value)
-                    return;
-
-                if (id != null && channelList.ContainsKey(id))
-                    channelList.Remove(id);
-
-                id = value;
-
-                if (id != null)
-                    channelList.Add(id, this);
-            }
-        }
+        public string ID { get; set; }
         public ChannelType Type { get; set; }
         [JsonProperty("display_name")]
         public string DisplayName { get; set; }
         public string Name { get; set; }
         public string Header { get; set; }
+
+        public Channel()
+        {
+            channelList.Add(this);
+        }
     }
 
     [JsonConverter(typeof(StringEnumConverter))]
